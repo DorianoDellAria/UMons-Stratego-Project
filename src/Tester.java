4ee1e65eea1f9c0c535dc7f93d3e2267f15af326
doridellaria@live.be <dWALt5FYrXn5>
2019-03-22 11:47:01 +0100

    update Board

--- src/Tester.java
+++ src/Tester.java
@@ -5,11 +5,7 @@ public class Tester {
 
 	public static void main(String[] args){
 		Board t=new Board();
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				//t.caseBoard[i][j] = j;
-			}
-		}
+		t.caseBoard[1][0].setContent(new Major(Team.Red));
 		t.display();
 	}
 
--- src/board/Board.java
+++ src/board/Board.java
@@ -1,14 +1,29 @@
 package board;
 
+import pieces.*;
+
 public class Board {
-    public Case [][] caseBoard = new Case [10][10];
+    public Case [][] caseBoard =new Case[10][10];
+
+    public Board(){
+        for(int i =0;i<caseBoard.length;i++){
+            for(int j=0;j<caseBoard[0].length;j++){
+                caseBoard[i][j]=new Case(null);
+            }
+        }
+    }
+
 
     public void display() {                 //Fonction d'affichage console pour debug
         for (int i = 0; i < 10; i++) {
-            System.out.print("|");
             for (int j = 0; j < 10; j++) {
-                System.out.print(caseBoard[i][j] + "|");
+                System.out.print("|");
+                if(caseBoard[i][j].getContent()!=null)
+                    System.out.print(caseBoard[i][j].getContent().getVALUE());
+                else
+                    System.out.print("null");
             }
+            System.out.print("|");
             System.out.println(" ");
         }
     }
