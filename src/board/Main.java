fdabf7893e53c85ff0fc08c2b5584688bdbe6973
doridellaria@live.be <dWALt5FYrXn5>
2019-04-20 16:38:11 +0200

    suppression direction

--- src/board/Main.java
+++ src/board/Main.java
@@ -9,6 +9,8 @@ import pieces.*;
 
 public class Main extends Application {
 
+	public static int nbCoup=0;
+
 	public static void main(String[] args){
 		launch(args);
 	}
@@ -29,11 +31,32 @@ public class Main extends Application {
 		Board.caseBoard[0][0].setContent(new Miner(Team.Red));
 		Board.caseBoard[0][1].setContent(new Miner(Team.Blue));
 		Board.caseBoard[0][6].setContent(new Obstacle());
+		Board.caseBoard[0][9].setContent(new Flag(Team.Blue));
+		Board.caseBoard[0][8].setContent(new Flag(Team.Red));
+
 
 
 
 		root.setCenter(board);
 
 		primaryStage.show();
+	}
+
+	public static void checkGameOver(){   //à optimiser ex: retenir la pace des flags pour eviter de faire un double for
+		Boolean redFlag=false;
+		Boolean blueFlag=false;
+		for(int i=0 ;i<10 ; i++){
+			for(int j = 0 ; j<10 ; j++){
+				if(Board.caseBoard[i][j].getContent() instanceof Flag && Board.caseBoard[i][j].getContent().team==Team.Blue)
+					blueFlag=true;
+				if(Board.caseBoard[i][j].getContent() instanceof Flag && Board.caseBoard[i][j].getContent().team==Team.Red)
+					redFlag=true;
+			}
+
+		}
+		if (blueFlag && redFlag)
+			System.out.println("ok");
+		else
+			System.out.println("gameOver");
 	}
 }
