a9abab0c66e9b9aa3f8b9598dc27ef01d5f7c5f6
doridellaria@live.be <dWALt5FYrXn5>
2019-04-22 21:20:02 +0200

    ajout de checkObstacle qui empeche les scout de passer au dessus des obstacles

--- src/pieces/Scout.java
+++ src/pieces/Scout.java
@@ -13,8 +13,8 @@ public class Scout extends AbstractMovable implements Movable{
 	@Override
 	public void move(int x1, int y1, int x2, int y2) {
 		if(Board.caseBoard[x2][y2].getContent()==null || Board.caseBoard[x1][y1].getContent().team!=Board.caseBoard[x2][y2].getContent().team && Board.caseBoard[x2][y2].getContent().team!=null ) {
-			if (x1 == x2 || y1 == y2) {
-				if (Board.caseBoard[x2][y2].getContent() == null) {
+			if ((x1 == x2 || y1 == y2) && checkObstacle(x1,y1,x2,y2)) {
+				if (Board.caseBoard[x2][y2].getContent() == null ) {
 					Board.caseBoard[x2][y2].setContent(this);
 				} else {
 					Board.caseBoard[x2][y2].setContent(this.fight(Board.caseBoard[x2][y2].getContent()));
@@ -22,6 +22,38 @@ public class Scout extends AbstractMovable implements Movable{
 				Board.caseBoard[x1][y1].setContent(null);
 			}
 		}
+	}
+
+	public boolean checkObstacle(int x1, int y1, int x2, int y2){
+		if (x1==x2){
+			if(y1<y2) {
+				for (int i = y1+1; i < y2; i++) {
+					if(Board.caseBoard[x1][i].getContent()!=null)
+						return false;
+				}
+			}
+			else{
+				for (int i = y1-1; i > y2; i--) {
+					if(Board.caseBoard[x1][i].getContent()!=null)
+						return false;
+				}
+			}
+		}
+		else {
+			if (x1 < x2) {
+				for (int i = x1+1; i < x2; i++) {
+					if (Board.caseBoard[i][y1].getContent() != null)
+						return false;
+				}
+			}
+			else {
+				for (int i = x1-1; i > x2; i--) {
+					if (Board.caseBoard[i][y1].getContent() != null)
+						return false;
+				}
+			}
+		}
+		return true;
 	}
 
 
