3807d5ed560ab9ffc63dd6670ef9771918b86139
thomasBren <48093219+thomasBren@users.noreply.github.com>
2019-05-06 16:03:49 +0200

    Update Colonel.java

--- src/pieces/Colonel.java
+++ src/pieces/Colonel.java
@@ -17,6 +17,8 @@ public class Colonel extends AbstractMovable implements Movable {
 			FileInputStream fis = new FileInputStream("./images/colonel.png");
 			Image tmp = new Image(fis);
 			this.img = new ImageView(tmp);
+			this.img.setFitHeight(45);
+			this.img.setPreserveRatio(true);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
